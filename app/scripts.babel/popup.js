f53d4d2408255250c5434bdf190ff1bcc8e6b3fe
Coacooo <naseeihility@outlook.com>
2016-10-24 23:01:25 +0800

    finish github link

--- app/scripts.babel/popup.js
+++ app/scripts.babel/popup.js
@@ -2,7 +2,11 @@
 
 const CRIED_ID = '79b284688b1aa407d535';
 var link = document.getElementById('link');
-link.addEventListener('click', function(e){
+var info = document.getElementById('info');
+var username = document.getElementById('username');
+link.addEventListener('click', linkGithub);
+
+function linkGithub(e) {
   e.preventDefault();
   var target = e.target;
   var url = target.href + CRIED_ID;
@@ -12,4 +16,49 @@ link.addEventListener('click', function(e){
   var left = (window.screen.availWidth - 10 - width) / 2;
   var position = `height=${height},width=${width},top=${top},left=${left},resizable=no`;
   window.open(url,'github',position);
-})
+}
+
+function domChange(usertoken) {
+  const token = usertoken;
+  const GITHUB_API = 'https://api.github.com/user?';
+  const url = `${GITHUB_API}access_token=${token}`;
+  ajax(url, (result) => {
+    var resultObj = JSON.parse(result);
+    var user_name = resultObj.name || '';
+    if (user_name !== '') {
+      link.classList.add('hide');
+      info.classList.remove('hide');
+      username.innerHTML = user_name;
+    }
+  });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  chrome.storage.sync.get('savetogistToken', (result) => {
+      result.savetogistToken && domChange(result.savetogistToken);
+  });
+
+  chrome.runtime.onMessage.addListener(function(message, sender, sendResponse){
+    if (message.token !== '') {
+      sendResponse('Success');
+      const token = message.token;
+      chrome.storage.sync.set({'savetogistToken': token}, function() {
+          domChange(token);
+      });
+    } else {
+      sendResponse('Error');
+    }
+  });
+
+});
+
+function ajax(url, callback) {
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", url, true);
+  xhr.send();
+  xhr.onreadystatechange = () =>{
+    if (xhr.readyState === 4) {
+      callback(xhr.responseText);
+    }
+  }
+}
---
+++ app/scripts.babel/token_content.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var hiddentoken = document.getElementById("hidden");
+var token = hiddentoken.dataset.key;
+if(token) {
+  sendToBackground(token);
+}
+
+function sendToBackground(token) {
+  chrome.runtime.sendMessage({token: token}, function(res){
+    if (res === 'Success'){
+      window.close();
+    } else {
+      console.log("Something wrong!");
+    }
+  })
+}
