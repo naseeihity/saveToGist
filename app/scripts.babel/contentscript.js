a2f78ab2cb53c33e83676027772c57f93a0de5b8
Coacooo <naseeihility@outlook.com>
2016-10-25 22:33:42 +0800

    a very base version done!!

--- app/scripts.babel/contentscript.js
+++ app/scripts.babel/contentscript.js
@@ -1,17 +1,21 @@
 'use strict';
 
 function getSelectionMessage(message, sender, sendResponse){
+  const copyFrom = `
+
+---------------------------F--R--O--M------------------------------
+${message.tab.url}
+-------------------------------------------------------------------`;
   let gist = {
-    url: message.tab.url,
     description: 'a description of this gist',
     public: true,
     "files": {
-      "demo.js": {
-        "content": message.text
+      "aGist": {
+        "content": message.text + copyFrom
       }
     }
   };
-  console.log(gist);
+
   chrome.storage.sync.get('savetogistToken', (result) => {
       let token = result.savetogistToken;
       showModal(gist, token);
@@ -22,9 +26,9 @@ function getSelectionMessage(message, sender, sendResponse){
 
 function showModal(gist, token) {
   // 弹框输入 description,public,filename
-  const modal = $(`<div class="gistModal">
+  const modal = $(`<div class="gistModal" id="gistModal">
       <div class="gistModal_content">
-        <form action="" class="gistModal_form">
+        <form action="" class="gistModal_form" id="gistModal_form" name="gistModal_form">
           <input
             type="text"
             id="gist_filename"
@@ -56,25 +60,50 @@ function showModal(gist, token) {
           </div>
         </form>
         <p class="infotips">If you leave it blank, we will use default values.</p>
-        <span class="closeBtn">X</span>
+        <span class="closeBtn" id="closeBtn">X</span>
       </div>
     </div>`);
+  // 弹出编辑弹框
     $('body').append(modal);
+  // 处理保存请求
     $("form.gistModal_form").submit(function(e){
       e.preventDefault();
+      $('.btnDone').prop('disabled', true);
+      let gistUrl;
+      //取到修改的值
+      const filename = $('#gist_filename').val().trim();
+      const description = $('#gist_description').val().trim();
+      const isPublic = $('#gist_public').prop('checked');
+
+      gist.description = description || gist.description;
+      gist.public = isPublic;
+      if(filename) {
+        gist.files[filename] = gist.files["aGist"];
+        delete gist.files["aGist"];
+      }
+      sendToGist(gist, token, (result) => {
+        gistUrl = result;
+        closeModal();
+      });
     });
-  // sendToGist(gist, token);
+
+    $('#closeBtn').on('click', function(event) {
+      event.preventDefault();
+      closeModal();
+    });
 }
 
-function showSuccessModal() {
-
+function closeModal() {
+  const modal = $('#gistModal');
+  modal.remove();
 }
 
-function sendToGist(gist, token) {
+function sendToGist(gist, token, callback) {
   const URL = `https://api.github.com/gists`;
   const tokenHeader = `token ${token}`;
   const data = JSON.stringify(gist);
-  console.log(data);
+  let gistUrl = '';
+
   $.ajax({
     url: URL,
     type: 'POST',
@@ -83,7 +112,8 @@ function sendToGist(gist, token) {
     },
     data: data,
     success: (res) => {
-      alert("success!");
+      gistUrl = res.html_url;
+      callback(gistUrl);
     },
     error: (err) => {
       console.log(err);
